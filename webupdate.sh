ffc7fe29ea644d84241a1a5af805e9e45c4feda0
root <atomand.space@gmail.com>
2019-09-29 11:18:00 +0000

    updated README.md

--- webupdate.sh
+++ webupdate.sh
@@ -1,8 +1,11 @@
 # updates the gihub with the current README.md file
 # This script is being run by cron 
 
-$(cd /root/firstweb/)
-$(git add README.md)
+cd /root/firstweb/
+echo "1"
+git add -A
 echo "updated"
-$(git commit -m "updated README.md")
-$(git push -u origin master)
+git commit -m "updated README.md"
+echo "2"
+git push -u origin master
+echo "3"
